9dc0b95feabb10377f3833018c27b05b8af0f8cb
Rym <bugarinrymuel@gmail.com>
2026-07-03 00:04:29 +0800

    validations added

--- schemas/student_profile_schema.py
+++ schemas/student_profile_schema.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator, model_validator, Field
 from utils.enum import StudentType, GradeLevel, UserSex
 from datetime import datetime
 
 class StudentProfileBase(BaseModel):
-    name: str
+    name: str = Field(min_length=5, max_length=100)
     age: int
     sex: UserSex
     grade_level: GradeLevel
@@ -13,6 +13,59 @@ class StudentProfileBase(BaseModel):
     guardians_contact_no: str | None = None
     address: str | None = None
 
+    @model_validator(mode="before")
+    @classmethod
+    def data_strip(cls, data):
+        if not isinstance(data, dict):
+            return data
+        
+        fields = ["name", "section", "guardians_name", "guardians_contact_no", "address"]
+        
+        for field in fields:
+            value = data.get(field)
+
+            if isinstance(value, str):
+                data[field] = value.strip()
+
+        return data
+    
+    @field_validator("name")
+    @classmethod
+    def validate_name(cls, value:str):
+        
+        if not value:
+            raise ValueError("Name cannot be empty")
+        
+        if not value[0].isupper():
+            raise ValueError("Name must start with an uppercase letter.")
+        
+        return value
+    
+    @field_validator("guardians_name")
+    @classmethod
+    def validate_guardians_name(cls, value:str):
+        
+        if value:
+            if not value[0].isupper():
+                raise ValueError("Guardians name must start with an uppercase letter.")
+        
+        return value
+    
+    @field_validator("guardians_contact_no")
+    @classmethod
+    def validate_guardians_contact_no(cls, value:str):
+
+        if not value.isdigit():
+            raise ValueError("Contact number must contain only numbers.")
+
+        if not value.startswith("0"):
+            raise ValueError("Contact number must start with 0.")
+        
+        if len(value) != 11:
+            raise ValueError("Contact number consists of 11 numbers.")
+
+        return value
+    
 class StudentProfileCreate(StudentProfileBase):
     pass
 
@@ -26,6 +79,64 @@ class StudentProfileUpdate(BaseModel):
     guardians_name: str | None = None
     guardians_contact_no: str | None = None
     address: str | None = None
+
+    @model_validator(mode="before")
+    @classmethod
+    def data_strip(cls, data):
+        if not isinstance(data, dict):
+            return data
+        
+        fields = ["name", "section", "guardians_name", "guardians_contact_no", "address"]
+        
+        for field in fields:
+            value = data.get(field)
+
+            if isinstance(value, str):
+                data[field] = value.strip()
+
+        return data
+    
+    @field_validator("name")
+    @classmethod
+    def validate_name(cls, value:str):
+        
+        if value is None:
+            return value
+        
+        if not value[0].isupper():
+            raise ValueError("Name must start with an uppercase letter.")
+        
+        return value
+    
+    @field_validator("guardians_name")
+    @classmethod
+    def validate_guardians_name(cls, value:str):
+        if value is None:
+            return value
+
+        if value:
+            if not value[0].isupper():
+                raise ValueError("Guardians name must start with an uppercase letter.")
+        
+        return value
+    
+    @field_validator("guardians_contact_no")
+    @classmethod
+    def validate_guardians_contact_no(cls, value:str):
+
+        if value is None:
+            return value
+
+        if not value.isdigit():
+            raise ValueError("Contact number must contain only numbers.")
+
+        if not value.startswith("0"):
+            raise ValueError("Contact number must start with 0.")
+        
+        if len(value) != 11:
+            raise ValueError("Contact number consists of 11 numbers.")
+
+        return value
 
 class StudentProfileOut(BaseModel):
     id: int
--- schemas/teacher_profile_schema.py
+++ schemas/teacher_profile_schema.py
@@ -1,27 +1,139 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr,Field, field_validator, model_validator
 from datetime import datetime
 from utils.enum import UserSex, GradeLevel
 
 class TeacherProfileBase(BaseModel):
-    name: str
+    name: str = Field(min_length=5, max_length=50)
     contact_no: str
-    age: int
+    age: int = Field(ge=18, le=100)
     sex: UserSex
 
     # sa ibang table to mag sesave si grade_level_handles
     grade_level_handles: list[GradeLevel]
-    address: str
+    address: str = Field(min_length=5,max_length=150)
+
+    @model_validator(mode="before")
+    @classmethod
+    def sanitize_inputs(cls, data):
+        if not isinstance(data, dict):
+            return data
+        
+        fields = ["name", "contact_no"]
+
+        for field in fields:
+            value = data.get(field)
+
+            if isinstance(value, str):
+                data[field] = value.strip()
+
+        return data
+    
+    @field_validator("name")
+    @classmethod
+    def validate_name(cls, value: str):
+        if not value:
+            raise ValueError("name cannot be empty")
+        
+        if not value[0].isupper():
+            raise ValueError("First letter must be capitalized")
+        
+        return value
+    
+    @field_validator("contact_no")
+    @classmethod
+    def validate_contact_no(cls, value: str):
+        if not value:
+            raise ValueError("please include contact no.")
+        
+        if not value.startswith("0"):
+            raise ValueError("Contact number must start with 0.")
+        
+        if not value.isdigit():
+            raise ValueError("Contact number must contain only numbers.")
+        
+        if len(value) != 11:
+            raise ValueError("Contact number consists of 11 numbers.")
+        
+        return value
+    
+    @field_validator("grade_level_handles")
+    @classmethod
+    def validate_grade_level_handles(cls, value):
+        if not value:
+            raise ValueError("include grade level handles")
+        
+        return value
 
 class TeacherProfileCreate(TeacherProfileBase):
     pass
 
 class TeacherProfileUpdate(BaseModel):
-    name: str | None = None
+    name: str | None = Field(None, min_length=5, max_length=50)
     contact_no: str | None = None
-    age: int | None = None
+    age: int | None = Field(None, ge=18, le=100)
     sex: UserSex | None = None
     grade_level_handles: list[GradeLevel] | None = None
-    address: str | None = None
+    address: str | None = Field(None, min_length=5, max_length=150)
+
+    @model_validator(mode="before")
+    @classmethod
+    def sanitize_inputs(cls, data):
+        if not isinstance(data, dict):
+            return data
+        
+        fields = ["name", "contact_no"]
+
+        for field in fields:
+            value = data.get(field)
+
+            if isinstance(value, str):
+                data[field] = value.strip()
+
+        return data
+    
+    @field_validator("name")
+    @classmethod
+    def validate_update_name(cls, value):
+        if value is None:
+            return value
+        
+        if not value[0].isupper():
+            raise ValueError("First letter must be capitalized")
+        
+        return value
+    
+    @field_validator("contact_no")
+    @classmethod
+    def validate_update_contact_no(cls, value):
+        if value is None:
+            return value
+        
+        if not value.startswith("0"):
+            raise ValueError("Contact number must start with 0.")
+        
+        if not value.isdigit():
+            raise ValueError("Contact number must contain only numbers.")
+        
+        if len(value) != 11:
+            raise ValueError("Contact number consists of 11 numbers.")
+        
+        return value
+    
+    @field_validator("grade_level_handles")
+    @classmethod
+    def validate_update_grade_level_handles(cls, value):
+        if value is None:
+            return value
+        
+        return value
+
+    @field_validator("address")
+    @classmethod
+    def validate_update_address(cls, value):
+        if value is None:
+            return value
+        
+        return value
 
 # Table to ng GradeLevel, para sa teacher_grade_handles
 class TeacherGradeHandlesOut(BaseModel):
