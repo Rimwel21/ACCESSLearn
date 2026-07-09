3d0c8f7905a1e36d518ea2dc1f42a7342f0b53dc
Rym <bugarinrymuel@gmail.com>
2026-07-09 20:19:13 +0800

    blocked by admin approval added

--- routes/admin_approval_route.py
+++ routes/admin_approval_route.py
@@ -2,13 +2,17 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from models.accounts import Accounts
 from utils.dependencies import get_current_user, get_db
-from services.admin_approval_service import admin_approval, teachers_pending_account
+from services.admin_approval_service import admin_approval, admin_block, teachers_pending_account
 
 router = APIRouter(prefix="/admin", tags=["Admin"])
 
 @router.patch("/teachers/{teacher_id}/approve")
 def admin_approval_routes(teacher_id: int, current_user: Accounts = Depends(get_current_user), db: Session = Depends(get_db)):
     return admin_approval(teacher_id=teacher_id, current_user=current_user, db=db)
+
+@router.patch("/teachers/{teacher_id}/block")
+def admin_block_routes(teacher_id: int, current_user: Accounts = Depends(get_current_user), db: Session = Depends(get_db)):
+    return admin_block(teacher_id=teacher_id, current_user=current_user, db=db)
 
 @router.get("/teachers/pendings")
 def teachers_pending_account_routes(current_user: Accounts = Depends(get_current_user), db: Session = Depends(get_db)):
--- services/admin_approval_service.py
+++ services/admin_approval_service.py
@@ -30,6 +30,25 @@ def admin_approval(teacher_id: int, current_user: Accounts, db: Session):
 
     return {"message": "teacher verified successfully!"}
 
+def admin_block(teacher_id: int, current_user: Accounts, db: Session):
+    if current_user.role != RoleEnum.admin:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Admin only")
+    
+    teacher = db.query(Accounts).filter(Accounts.id == teacher_id, Accounts.role == RoleEnum.teacher).first()
+
+    if not teacher:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Teacher account not found")
+    
+    if teacher.verification_status == VerificationStatus.blocked:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="This teacher account has already been blocked.")
+
+    teacher.verification_status = VerificationStatus.blocked
+
+    db.commit()
+    db.refresh(teacher)
+
+    return {"message": "teacher blocked successfully!"}
+
 def teachers_pending_account(current_user: Accounts, db: Session):
     if current_user.role != RoleEnum.admin:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Admin only")
